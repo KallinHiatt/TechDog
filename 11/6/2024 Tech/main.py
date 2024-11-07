e704f523aa1a726da4011d804b130219d908a1d7
KallinHiatt <kallin.hiatt@ucas-edu.net>
2024-11-07 11:05:18 -0700

    first commit

---
+++ 11/6/2024 Tech/main.py
@@ -0,0 +1,6 @@
+from tech import Tech, Phone, Laptop
+
+print(Phone("Galaxy", 128, "sage"))
+print(repr(Phone("Galaxy", 128, "sage")))
+print(Laptop("MackBook Pro", 256, 15))
+print(repr(Laptop("MackBook Pro", 256, 15)))
